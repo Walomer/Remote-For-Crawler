0602de0658ac0e39de6361b71d8b67dc4fd13000
SylvainVassr <sylvain.vasseur17@gmail.com>
2022-01-28 15:06:59 +0100

    Ajout du style - form - scraper

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from nis import match
 import sqlite3
 from werkzeug.exceptions import abort
 from flask import Flask, render_template, request
@@ -5,6 +6,9 @@ import sys
 import snscrape.modules.twitter as twitterScraper
 
 app = Flask(__name__)
+
+# Liste des tweets que l'on va recuperer -> JSON file
+tweets = []
 
 
 @app.route('/')
@@ -17,26 +21,46 @@ def home():
 
 @app.route('/research', methods=['POST'])
 def research():
-    #! Gerer les required sur le champ du form pour que request.form ait toujours la meme taille
-    print(request.form.to_dict(flat=False))
-    typeSearch = list(request.form.to_dict(flat=False).keys())[1]
-    search = request.form.get('search')
+    #! afficher les tweet dans article selon bdd
+    #! regarder si possibilite de stop, reprendre, 10 avant, 10 apres (la télécommande)
+    # ? Meiller facon pour refresh c'est de call AJAX. fonction avec timer sur notre JSON et on recup le res de al taille
 
+    print(request.form.to_dict(flat=False))
+    search = request.form.get('search')
+    typeSearch = request.form.get('typeRecherche')
+    qte = request.form.get('qte_tweet')
+
+    # Si on arrive ici, c'est un nouvelle recherche, dont on réinitialise notre liste
     tweets = []
+
+    # On filtre selon le form
     if typeSearch == "typeUsers":
-        scraper = twitterScraper.TwitterUserScraper("KermitTheFrog", False)
-        for i, tweet in enumerate(scraper.get_items()):
-            tweets.append({"id": tweet.id, "user": tweet.user, "content": tweet.content,
-                          "reply": tweet.replyCount, "retweet": tweet.retweetCount,
-                           "likes": tweet.likeCount, "langue": tweet.lang, "date": tweet.date})
-        msg = search+" by users"
+        scraper = twitterScraper.TwitterUserScraper(search, False)
     elif typeSearch == "typeHashtag":
-        # twitterScraper.TwitterHashtagScraper()
-        msg = search+" by #"
+        scraper = twitterScraper.TwitterHashtagScraper(search)
     else:
-        # twitterScraper.TwitterSearchScraper()
-        msg = search+" a determiner"
-    return render_template('home.html', toprint=msg)
+        scraper = twitterScraper.TwitterSearchScraper(search)
+
+    # Des qu'on arrive à la qte demandé, on s'arrete
+    if qte != 0:
+        for i, tweet in enumerate(scraper.get_items()):
+            if i > qte:
+                break
+            tweets.append({"id": tweet.id, "user": tweet.user.username, "content": tweet.content,
+                           "reply": tweet.replyCount, "retweet": tweet.retweetCount,
+                           "likes": tweet.likeCount, "langue": tweet.lang, "date": tweet.date})
+    # Pas de qte donne, on prends donc tout les tweet correspodant
+    else:
+        for i, tweet in enumerate(scraper.get_items()):
+            tweets.append({"id": tweet.id, "user": tweet.user.username, "content": tweet.content,
+                           "reply": tweet.replyCount, "retweet": tweet.retweetCount,
+                           "likes": tweet.likeCount, "langue": tweet.lang, "date": tweet.date})
+
+    # * Ici on ajoute dans la BD
+    #! Formatter la date en yyyy-mm-jj
+
+    # print(tweets)
+    return render_template('home.html', ans=tweets)
 
 
 def get_db_connection():
